46ab9df229bbd48683b21ba7edc893daf857aa11
Hui Tang <huitang.pku@gmail.com>
2023-03-14 13:01:41 +0200

    finalizing the multiple functionalities for phydro

--- src/phydro_mod.f90
+++ src/phydro_mod.f90
@@ -142,7 +142,7 @@ contains
 }
 
   SUBROUTINE calc_kmm(tc, patm, kmm)
-    !
+    !-------------------------------------------------------------------------
     ! !DESCRIPTION:
     ! Calculates the Michaelis Menten coefficient for Rubisco-limited photosynthesis
     ! Calculates the Michaelis Menten coefficient of Rubisco-limited assimilation as a function of temperature and atmospheric pressure.
@@ -156,7 +156,8 @@ contains
     !    R.  J.,  and  Long,  S.  P.:Improved temperature response functions
     !    for models of Rubisco-limited photosyn-thesis, Plant, Cell and
     !    Environment, 24, 253–259, 2001
-    
+    !--------------------------------------------------------------------------
+
     ! !USES:
     use readpara_mod   
 
@@ -212,273 +213,202 @@ contains
   
   END FUNCTION ftemp_arrh
 
-  real(r8) FUNCTION gammastar(tk, dha, tkref)
+  real(r8) FUNCTION gammastar(tc, patm)
+    
+    !---------------------------------------------------------------
+    ! Calculates the CO2 compensation point
+    ! Calculates the photorespiratory CO2 compensation point in absence of dark
+    ! respiration, \eqn{\Gamma*} (Farquhar, 1980).
+    ! Temperature and pressure-dependent
+    !---------------------------------------------------------------
 
-   Calculates the CO2 compensation point
-#'
-#' Calculates the photorespiratory CO2 compensation point in absence of dark
-#' respiration, \eqn{\Gamma*} (Farquhar, 1980).
-#'
-#' @param tc Temperature, relevant for photosynthesis (degrees Celsius)
-#' @param patm Atmospheric pressure (Pa)
-#'
-#' @details The temperature and pressure-dependent photorespiratory
-#' compensation point in absence of dark respiration \eqn{\Gamma* (T,p)}
-#' is calculated from its value at standard temperature (\eqn{T0 = 25 }deg C)
-#' and atmospheric pressure (\eqn{p0 = 101325} Pa), referred to as \eqn{\Gamma*0},
-#' quantified by Bernacchi et al. (2001) to 4.332 Pa (their value in molar
-#' concentration units is multiplied here with 101325 Pa to yield 4.332 Pa).
-#' \eqn{\Gamma*0} is modified by temperature following an Arrhenius-type temperature
-#' response function \eqn{f(T, \Delta Ha)} (implemented by \link{ftemp_arrh})
-#' with activation energy \eqn{\Delta Ha = 37830} J mol-1  and is corrected for
-#' atmospheric pressure \eqn{p(z)} (see \link{calc_patm}) at elevation \eqn{z}.
-#' \deqn{
-#'       \Gamma* = \Gamma*0 f(T, \Delta Ha) p(z) / p_0
-#' }
-#' \eqn{p(z)} is given by argument \code{patm}.
-#'
-#' @references Farquhar,  G.  D.,  von  Caemmerer,  S.,  and  Berry,  J.  A.:
-#'             A  biochemical  model  of photosynthetic CO2 assimilation in leaves of
-#'             C 3 species, Planta, 149, 78–90, 1980.
-#'
-#'             Bernacchi,  C.  J.,  Singsaas,  E.  L.,  Pimentel,  C.,  Portis,  A.
-#'             R.  J.,  and  Long,  S.  P.:Improved temperature response functions
-#'             for models of Rubisco-limited photosyn-thesis, Plant, Cell and
-#'             Environment, 24, 253–259, 2001
-#'
-#' @return A numeric value for \eqn{\Gamma*} (in Pa)
-#'
-#' @examples print("CO2 compensation point at 20 degrees Celsius and standard atmosphere (in Pa):")
-#' print(calc_gammastar(20, 101325))
-#'
-#' @export
-#'
-calc_gammastar <- function( tc, patm ) {
+    ! !ARGUMENTS
+    real(r8), intent(in) :: tc                        ! Temperature, relevant for photosynthesis (degrees Celsius)
+    real(r8), intent(in) :: patm                      ! Atmospheric pressure (Pa)
+    
+    ! !LOCAL VARIABLES:
+    real(r8)             ::  dha    = 37830           ! Activation energy (J/mol), Bernacchi et al. (2001)
+    real(r8)             ::  gs25_0 = 4.332           ! Photorespiratory CO2 compensation point at standard temperature 
+                                                      ! (T = 25 degC, p0 = 101325) (Pa)
+                                                      ! Quantified by Bernacchi et al. (2001) to 4.332 Pa 
+                                                      ! Their value in molar concentration units is multiplied with 101325 Pa to yield 4.332 Pa                                                 
+    real(r8)             ::  patm0 = 101325           ! Atmospheric pressure at sea level (Pa), defaults to 101325 Pa.
+
+    gammastar = gs25_0 * patm / patm0 * ftemp_arrh((tc + 273.15), dha=dha)
   
-  # (J/mol) Activation energy, Bernacchi et al. (2001)
-  dha    <- 37830
-  
-  # Pa, value based on Bernacchi et al. (2001), 
-  # converted to Pa by T. Davis assuming elevation of 227.076 m.a.s.l.
-  gs25_0 <- 4.332
-  
-  gammastar <- gs25_0 * patm / calc_patm(0.0) * ftemp_arrh( (tc + 273.15), dha=dha )
-  
-  return( gammastar )
-}
+    return
   
   END FUNCTION gammastar
   
-  real(r8) FUNCTION ftemp_kphio(tk, dha, tkref)
+  real(r8) FUNCTION ftemp_kphio(tc, c4 = FALSE )
+    
+    !---------------------------------------------------------------
+    ! Calculates the temperature dependence of the quantum yield efficiency.
+    ! following the temperature dependence of the maximum quantum yield of photosystem II
+    ! in light-adapted tobacco leaves, determined by Bernacchi et al. (2003).
+    ! The factor is to be multiplied with leaf absorptance and the fraction
+    ! of absorbed light that reaches photosystem II.
+    ! From function "ftemp_kphio" in p-model
+    !---------------------------------------------------------------
 
-    #' Calculates the temperature dependence of the quantum yield efficiency
-#'
-#' Calculates the temperature dependence of the quantum yield efficiency
-#' following the temperature dependence of the maximum quantum yield of photosystem II
-#' in light-adapted tobacco leaves, determined by Bernacchi et al. (2003)
-#'
-#' @param tc Temperature, relevant for photosynthesis (degrees Celsius)
-#' @param c4 Boolean specifying whether fitted temperature response for C4 plants
-#' is used. Defaults to \code{FALSE} (C3 photoynthesis temperature resposne following
-#' Bernacchi et al., 2003 is used).
-#'
-#' @details The temperature factor for C3 photosynthesis (argument \code{c4 = FALSE}) is calculated
-#' based on Bernacchi et al. (2003) as
-#' 			\deqn{
-#' 				\phi(T) = 0.352 + 0.022 T - 0.00034 T^2
-#'       }
-#'
-#' The temperature factor for C4 (argument \code{c4 = TRUE}) photosynthesis is calculated based on
-#' pers. comm. by David Orme, correcting values provided in Cai & Prentice (2020). Corrected 
-#' parametrisation is:
-#' 			\deqn{
-#' 				\phi(T) = -0.064 + 0.03 T - 0.000464 T^2 
-#'       }
-#'
-#' The factor \eqn{\phi(T)} is to be multiplied with leaf absorptance and the fraction
-#' of absorbed light that reaches photosystem II. In the P-model these additional factors
-#' are lumped into a single apparent quantum yield efficiency parameter (argument \code{kphio}
-#' to function \link{rpmodel}).
-#'
-#' @return A numeric value for \eqn{\phi(T)}
-#'
-#' @examples
-#' ## Relative change in the quantum yield efficiency
-#' ## between 5 and 25 degrees celsius (percent change):
-#' print(paste((ftemp_kphio(25.0)/ftemp_kphio(5.0)-1)*100 ))
-#'
-#' @references  
-#' Bernacchi, C. J., Pimentel, C., and Long, S. P.:  In vivo temperature
-#' 				response func-tions  of  parameters required  to  model  RuBP-limited
-#' 				photosynthesis,  Plant  Cell Environ., 26, 1419–1430, 2003
-#' Cai, W., and Prentice, I. C.: Recent trends in gross primary production 
-#'        and their drivers: analysis and modelling at flux-site and global scales,
-#'        Environ. Res. Lett. 15 124050 https://doi.org/10.1088/1748-9326/abc64e, 2020
-#'
-#' @export
-#'
-ftemp_kphio <- function( tc, c4 = FALSE ){
+    ! !ARGUMENTS
+    real(r8), intent(in) :: tc                        ! Temperature, relevant for photosynthesis (degrees Celsius)
+    logical,  intent(in) :: c4                        ! Boolean specifying whether fitted temperature response for C4 plants
+                                                      ! Defaults to FALSE (C3 photoynthesis temperature resposne following
+                                                      ! Bernacchi et al., 2003 is used
+
+    if (c4) then
+      ! correcting erroneous values provided in Cai & Prentice, 2020, according to D. Orme (issue #19) 
+      ! XXX THIS IS NOT CORRECT: ftemp = -0.008 + 0.00375 * tc - 0.58e-4 * tc^2   # Based on calibrated values by Shirley
+      ftemp_kphio = -0.064 + 0.03 * tc - 0.000464 * tc^2     
+    else 
+      ! The temperature factor for C3 photosynthesis (c4 = FALSE) is calculated based on Bernacchi et al. (2003)
+      ftemp_kphio = 0.352 + 0.022 * tc - 3.4e-4 * tc^2
+    
+    end if
   
-  if (c4){
-    ftemp = -0.064 + 0.03 * tc - 0.000464 * tc^2     # correcting erroneous values provided in Cai & Prentice, 2020, according to D. Orme (issue #19) 
-    # XXX THIS IS NOT CORRECT: ftemp = -0.008 + 0.00375 * tc - 0.58e-4 * tc^2   # Based on calibrated values by Shirley
-  } else {
-    ftemp <- 0.352 + 0.022 * tc - 3.4e-4 * tc^2
-  }
-  
-  ## avoid negative values
-  ftemp <- ifelse(ftemp < 0.0, 0.0, ftemp)
-  
-  return(ftemp)
-}
-  
+    ! Avoid negative values
+    if (ftemp_kphio<0.0) then
+      ftemp_kphio=0.0
+    end if
+
+    return
+
   END FUNCTION ftemp_kphio
-  
-  real(r8) FUNCTION viscosity_h2o(tk, dha, tkref)
 
-#' Viscosity of water
-#'
-#' Calculates the viscosity of water as a function of temperature and atmospheric
-#' pressure.
-#'
-#' @param tc numeric, air temperature (tc), degrees C
-#' @param p numeric, atmospheric pressure (p), Pa
-#'
-#' @return numeric, viscosity of water (mu), Pa s
-#'
-#' @examples print("Density of water at 20 degrees C and standard atmospheric pressure:")
-#' print(density_h2o(20, 101325))
-#'
-#' @references  Huber, M. L., R. A. Perkins, A. Laesecke, D. G. Friend, J. V.
-#' Sengers, M. J. Assael, ..., K. Miyagawa (2009) New
-#' international formulation for the viscosity of H2O, J. Phys.
-#' Chem. Ref. Data, Vol. 38(2), pp. 101-125.
-#'
-#' @export
-#'
-viscosity_h2o <- function(tc, p) {
-  
-  # Define reference temperature, density, and pressure values:
-  tk_ast  <- 647.096    # Kelvin
-  rho_ast <- 322.0      # kg/m^3
-  mu_ast  <- 1e-6       # Pa s
-  
-  # Get the density of water, kg/m^3
-  rho <- density_h2o(tc, p)
-  
-  # Calculate dimensionless parameters:
-  tbar  <- (tc + 273.15)/tk_ast
-  tbarx <- tbar^(0.5)
-  tbar2 <- tbar^2
-  tbar3 <- tbar^3
-  rbar  <- rho/rho_ast
-  
-  # Calculate mu0 (Eq. 11 & Table 2, Huber et al., 2009):
-  mu0 <- 1.67752 + 2.20462/tbar + 0.6366564/tbar2 - 0.241605/tbar3
-  mu0 <- 1e2*tbarx/mu0
-  
-  # Create Table 3, Huber et al. (2009):
-  h_array <- array(0.0, dim=c(7,6))
-  h_array[1,] <- c(0.520094, 0.0850895, -1.08374, -0.289555, 0.0, 0.0)  # hj0
-  h_array[2,] <- c(0.222531, 0.999115, 1.88797, 1.26613, 0.0, 0.120573) # hj1
-  h_array[3,] <- c(-0.281378, -0.906851, -0.772479, -0.489837, -0.257040, 0.0) # hj2
-  h_array[4,] <- c(0.161913,  0.257399, 0.0, 0.0, 0.0, 0.0) # hj3
-  h_array[5,] <- c(-0.0325372, 0.0, 0.0, 0.0698452, 0.0, 0.0) # hj4
-  h_array[6,] <- c(0.0, 0.0, 0.0, 0.0, 0.00872102, 0.0) # hj5
-  h_array[7,] <- c(0.0, 0.0, 0.0, -0.00435673, 0.0, -0.000593264) # hj6
-  
-  # Calculate mu1 (Eq. 12 & Table 3, Huber et al., 2009):
-  mu1 <- 0.0
-  ctbar <- (1.0/tbar) - 1.0
-  # print(paste("ctbar",ctbar))
-  # for i in xrange(6):
-  for (i in 1:6){
-    coef1 <- ctbar^(i-1)
-    # print(paste("i, coef1", i, coef1))
-    coef2 <- 0.0
-    for (j in 1:7){
-      coef2 <- coef2 + h_array[j,i] * (rbar - 1.0)^(j-1)
-    }
-    mu1 <- mu1 + coef1 * coef2
-  }
-  mu1 <- exp( rbar * mu1 )
-  # print(paste("mu1",mu1))
-  
-  # Calculate mu_bar (Eq. 2, Huber et al., 2009)
-  #   assumes mu2 = 1
-  mu_bar <- mu0 * mu1
-  
-  # Calculate mu (Eq. 1, Huber et al., 2009)
-  mu <- mu_bar * mu_ast    # Pa s
-  
-  return( mu )
-}
 
+  real(r8) FUNCTION viscosity_h2o(tc, patm)
+
+    !---------------------------------------------------------------
+    ! Calculates the viscosity of water ((mu), Pa s) as a function of temperature and atmospheric pressure.
+    ! From function "viscosity_h2o" in p-model
+    ! References:
+    ! Huber, M. L., R. A. Perkins, A. Laesecke, D. G. Friend, J. V. Sengers, M. J. Assael, ..., K. Miyagawa (2009) 
+    !    New international formulation for the viscosity of H2O, J. Phys. Chem. Ref. Data, Vol. 38(2), pp. 101-125.
+    !---------------------------------------------------------------
+
+    ! !ARGUMENTS
+    real(r8), intent(in) :: tc                        ! Air temperature, degrees C
+    real(r8), intent(in) :: patm                      ! Atmospheric pressure (Pa)
+
+    ! ! Local variables 
+    ! Define reference temperature, density, and pressure values
+    real(r8)     ::    tk_ast  = 647.096    ! Kelvin
+    real(r8)     ::    rho_ast = 322.0      ! kg/m^3
+    real(r8)     ::    mu_ast  = 1e-6       ! Pa s
+    real(r8)     ::    rho                  ! density of water, kg/m^3
+    real(r8)     ::    tbar, tbarx, tbar2, tbar3, rbar, ctbar     ! dimensionless parameters
+    real(r8)     ::    coef1,
+    real(r8)     ::    mu0, mu1            !
+    
+    real(r8), dimension (1:7,1:6)     ::    h_array     ! Create Table 3, Huber et al. (2009)
+
+    integer      ::    i, j
+
+    ! Get the density of water, kg/m^3
+    rho = density_h2o(tc, patm)
+  
+    ! Calculate dimensionless parameters:
+    tbar  = (tc + 273.15)/tk_ast
+    tbarx = tbar^(0.5)
+    tbar2 = tbar^2
+    tbar3 = tbar^3
+    rbar  = rho/rho_ast
+  
+    ! Calculate mu0 (Eq. 11 & Table 2, Huber et al., 2009):
+    mu0 = 1.67752 + 2.20462/tbar + 0.6366564/tbar2 - 0.241605/tbar3
+    mu0 = 1e2*tbarx/mu0
+  
+    ! Create Table 3, Huber et al. (2009):
+
+    h_array(1,:) = (/0.520094, 0.0850895, -1.08374, -0.289555, 0.0, 0.0/)  ! hj0
+    h_array(2,:) = (/0.222531, 0.999115, 1.88797, 1.26613, 0.0, 0.120573/) ! hj1
+    h_array(3,:) = (/-0.281378, -0.906851, -0.772479, -0.489837, -0.257040, 0.0/) ! hj2
+    h_array(4,:) = (/0.161913,  0.257399, 0.0, 0.0, 0.0, 0.0/) ! hj3
+    h_array(5,:) = (/-0.0325372, 0.0, 0.0, 0.0698452, 0.0, 0.0/) ! hj4
+    h_array(6,:) = (/0.0, 0.0, 0.0, 0.0, 0.00872102, 0.0/) ! hj5
+    h_array(7,:) = (/0.0, 0.0, 0.0, -0.00435673, 0.0, -0.000593264/) ! hj6
+  
+    ! Calculate mu1 (Eq. 12 & Table 3, Huber et al., 2009):
+    mu1 = 0.0
+    ctbar = (1.0/tbar) - 1.0
+
+    do i=1,6
+      coef1 = ctbar^(i-1)
+      coef2 = 0.0
+      do j=1,7
+        coef2 = coef2 + h_array(j,i) * (rbar - 1.0)^(j-1)
+      end do
+      mu1 = mu1 + coef1 * coef2
+    end do
+
+    mu1 = exp(rbar*mu1)
+  
+    ! Calculate mu_bar (Eq. 2, Huber et al., 2009)
+    !  assumes mu2 = 1
+    mu_bar = mu0 * mu1
+  
+    ! Calculate mu (Eq. 1, Huber et al., 2009)
+    viscosity_h2o = mu_bar * mu_ast    ! Pa s
   
     return
   
   END FUNCTION viscosity_h2o
   
-  
-  #' Density of water
-#'
-#' Calculates the density of water as a function of temperature and atmospheric
-#' pressure, using the Tumlirz Equation.
-#'
-#' @param tc numeric, air temperature (tc), degrees C
-#' @param p numeric, atmospheric pressure (p), Pa
-#'
-#' @return numeric, density of water, kg/m^3
-#'
-#' @examples 
-#'  # Density of water at 20 degrees C and standard atmospheric pressure
-#'  print(density_h2o(20, 101325))
-#'
-#' @references  F.H. Fisher and O.E Dial, Jr. (1975) Equation of state of
-#' pure water and sea water, Tech. Rept., Marine Physical
-#' Laboratory, San Diego, CA.
-#'
-#' @export
-#'
-density_h2o <- function(tc, p){
-  
-  # Calculate lambda, (bar cm^3)/g:
-  my_lambda <- 1788.316 +
-    21.55053*tc +
-    -0.4695911*tc*tc +
-    (3.096363e-3)*tc*tc*tc +
-    -(7.341182e-6)*tc*tc*tc*tc
-  
-  # Calculate po, bar
-  po <- 5918.499 +
-    58.05267*tc +
-    -1.1253317*tc*tc +
-    (6.6123869e-3)*tc*tc*tc +
-    -(1.4661625e-5)*tc*tc*tc*tc
-  
-  # Calculate vinf, cm^3/g
-  vinf <- 0.6980547 +
-    -(7.435626e-4)*tc +
-    (3.704258e-5)*tc*tc +
-    -(6.315724e-7)*tc*tc*tc +
-    (9.829576e-9)*tc*tc*tc*tc +
-    -(1.197269e-10)*tc*tc*tc*tc*tc +
-    (1.005461e-12)*tc*tc*tc*tc*tc*tc +
-    -(5.437898e-15)*tc*tc*tc*tc*tc*tc*tc +
-    (1.69946e-17)*tc*tc*tc*tc*tc*tc*tc*tc +
-    -(2.295063e-20)*tc*tc*tc*tc*tc*tc*tc*tc*tc
-  
-  # Convert pressure to bars (1 bar <- 100000 Pa)
-  pbar <- (1e-5)*p
-  
-  # Calculate the specific volume (cm^3 g^-1):
-  v <- vinf + my_lambda/(po + pbar)
-  
-  # Convert to density (g cm^-3) -> 1000 g/kg; 1000000 cm^3/m^3 -> kg/m^3:
-  rho <- (1e3/v)
-  
-  return(rho)
-}
+  real(r8) FUNCTION density_h2o(tc, patm)
 
+    !---------------------------------------------------------------
+    ! Calculates the density of water (kg/m^3) as a function of temperature and atmospheric pressure, using the Tumlirz Equation.
+    ! From function "viscosity_h2o" in p-model
+    ! References:
+    ! F.H. Fisher and O.E Dial, Jr. (1975) Equation of state of pure water and sea water, Tech. Rept., Marine Physical Laboratory, San Diego, CA.
+    !---------------------------------------------------------------
+
+    ! !ARGUMENTS
+    real(r8), intent(in) :: tc                        ! Air temperature, degrees C
+    real(r8), intent(in) :: patm                      ! Atmospheric pressure (Pa)
+
+    ! ! Local variables 
+    real(r8)     ::    my_lambda    ! lambda, (bar cm^3)/g
+    real(r8)     ::    po           ! bar
+    real(r8)     ::    vinf, v      ! cm^3/g
+    real(r8)     ::    pbar         ! pressure to bars
+  
+    ! Calculate lambda, (bar cm^3)/g
+    my_lambda = 1788.316 + 21.55053*tc +  &
+                -0.4695911*tc*tc + (3.096363e-3)*tc*tc*tc + &
+                -(7.341182e-6)*tc*tc*tc*tc
+  
+    ! Calculate po, bar
+    po = 5918.499 +  &
+          58.05267*tc + &
+          -1.1253317*tc*tc + &
+          (6.6123869e-3)*tc*tc*tc + &
+          -(1.4661625e-5)*tc*tc*tc*tc
+  
+    ! Calculate vinf, cm^3/g
+    vinf = 0.6980547 + &
+           -(7.435626e-4)*tc + &
+          (3.704258e-5)*tc*tc + &
+          -(6.315724e-7)*tc*tc*tc + &
+          (9.829576e-9)*tc*tc*tc*tc + &
+          -(1.197269e-10)*tc*tc*tc*tc*tc + &
+          (1.005461e-12)*tc*tc*tc*tc*tc*tc + &
+          -(5.437898e-15)*tc*tc*tc*tc*tc*tc*tc + &
+          (1.69946e-17)*tc*tc*tc*tc*tc*tc*tc*tc + &
+          -(2.295063e-20)*tc*tc*tc*tc*tc*tc*tc*tc*tc
+  
+    ! Convert pressure to bars (1 bar <- 100000 Pa)
+    pbar = (1e-5)*p
+  
+    ! Calculate the specific volume (cm^3 g^-1):
+    v = vinf + my_lambda/(po + pbar)
+  
+    ! Convert to density (g cm^-3) -> 1000 g/kg; 1000000 cm^3/m^3 -> kg/m^3:
+    density_h2o = (1e3/v)
+
+    return
+
+  END FUNCTION density_h2o
   
 end module phydro_mod
