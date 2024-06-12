a130e706e04ced61b480fe8085d299496f0bb484
Hui Tang <tang@fmi-pecan.fmi.fi>
2024-06-12 15:47:10 +0300

    resolve the warning during compiling:Unary operator following arithmetic operator (use parentheses)

--- src/phydro_mod.f90
+++ src/phydro_mod.f90
@@ -565,27 +565,27 @@ contains
   
     ! Calculate lambda, (bar cm^3)/g
     my_lambda = 1788.316 + 21.55053*tc +  &
-                -0.4695911*tc*tc + (3.096363e-3)*tc*tc*tc + &
-                -(7.341182e-6)*tc*tc*tc*tc
+                (-0.4695911)*tc*tc + (3.096363e-3)*tc*tc*tc + &
+                (-7.341182e-6)*tc*tc*tc*tc
   
     ! Calculate po, bar
     po = 5918.499 +  &
           58.05267*tc + &
-          -1.1253317*tc*tc + &
+          (-1.1253317)*tc*tc + &
           (6.6123869e-3)*tc*tc*tc + &
-          -(1.4661625e-5)*tc*tc*tc*tc
+          (-1.4661625e-5)*tc*tc*tc*tc
   
     ! Calculate vinf, cm^3/g
     vinf = 0.6980547 + &
-           -(7.435626e-4)*tc + &
+           (-7.435626e-4)*tc + &
           (3.704258e-5)*tc*tc + &
-          -(6.315724e-7)*tc*tc*tc + &
+          (-6.315724e-7)*tc*tc*tc + &
           (9.829576e-9)*tc*tc*tc*tc + &
-          -(1.197269e-10)*tc*tc*tc*tc*tc + &
+          (-1.197269e-10)*tc*tc*tc*tc*tc + &
           (1.005461e-12)*tc*tc*tc*tc*tc*tc + &
-          -(5.437898e-15)*tc*tc*tc*tc*tc*tc*tc + &
+          (-5.437898e-15)*tc*tc*tc*tc*tc*tc*tc + &
           (1.69946e-17)*tc*tc*tc*tc*tc*tc*tc*tc + &
-          -(2.295063e-20)*tc*tc*tc*tc*tc*tc*tc*tc*tc
+          (-2.295063e-20)*tc*tc*tc*tc*tc*tc*tc*tc*tc
   
     ! Convert pressure to bars (1 bar <- 100000 Pa)
     pbar = (1e-5)*patm
