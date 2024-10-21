0030eaa5ae02a2d1f533db9af3d49f5b1b6dc41c
Hui Tang <huitang.pku@gmail.com>
2024-10-21 12:45:07 +0300

    initial commit of run script

---
+++ scripts/Single_run_haa00.sh
@@ -0,0 +1,189 @@
+#!/bin/bash
+
+para1=$1
+para2=$2
+para3=$3
+para4=$4
+expid=$5
+run_case=$6            # T or F, swtich for running long experiments
+
+echo "para1: $para1"
+echo "para2: $para2"
+echo "para3: $para3"
+echo "para4: $para4"
+echo "expid: $expid"
+echo "run_case: $run_case"
+
+mkdir -p /projappl/project_2006422/svm_data/output/veg_sensitivty/$expid
+
+######### Set "run_***" to "T" to run the site simulation
+post_proc="F"         # T or F, swtich for archiving experiments. 
+
+if [ ${run_case} == "T" ]
+then
+
+module load gcc/11.3.0 openmpi/4.1.4 netcdf-fortran/4.5.4
+
+cd /projappl/project_2006422/svm_data/output/veg_sensitivty/$expid
+
+cat << EOF > ctrl_namelist
+&ctrl_namelist
+start_date_day=20230101
+start_date_hour=000000
+time_step=1
+end_date_day=20240801
+end_date_hour=000000
+num_sites=1
+sites_name='haa00'
+input_dir='/users/tanghui1/SVMC/data/input/'  
+output_dir='/projappl/project_2006422/svm_data/output/uvidi_sites/'     
+time_step_output=1.0
+obs_lai=.true.
+obs_soilmoist=.false.
+obs_snowdepth=.false.
+obs_manage=.true.
+yasso_year=.false.
+phydro_debug=.false.
+yasso_debug=.false.
+water_debug=.false.
+log_level=1
+experiment_id='vegsen_${expid}'
+/
+EOF
+
+cat << EOF > veg_namelist
+&veg_namelist
+num_pft=1
+pft_type="oat"
+conductivity=${para1}
+psi50=${para2}
+b=2
+alpha=${para3}
+gamma=${para4}
+opt_hypothesis='PM'
+rdark=0.015
+/
+EOF
+
+cat << EOF > alloc_namelist
+&alloc_namelist
+ cratio_resp=5.0e-8
+ cratio_leaf=0.6
+ cratio_root=0.4
+ cratio_biomass=0.42
+ harvest_index=0.8
+ turnover_cleaf=0.03 
+ turnover_croot=0.004
+ sla=10
+ q10=2
+ invert_option=0
+/
+EOF
+
+cat << EOF > soilhydro_namelist
+&soilhydro_namelist
+ soil_depth=0.6
+ max_poros=0.54
+ fc=0.29
+ wp=0.09
+ ksat=2.0e-6
+ org_depth=0.04 
+ org_poros=0.9
+ org_fc=0.3 
+ maxpond=0.0
+ org_sat=1.0   
+ n_van=1.18     
+ watres=0.0
+ alpha_van=3.35
+ watsat=0.54
+ wmax=0.5
+ wmaxsnow=4.5
+ hc=0.6
+ w_leaf=0.01
+ rw=0.20
+ rwmin=0.02
+ gsoil=5e-3
+ kmelt=2.8934e-05
+ kfreeze=5.79e-6
+ frac_snowliq=0.05
+ zmeas=2.0
+ zground=0.1
+ zo_ground=0.01
+/
+EOF
+
+cat << EOF > soilyasso_namelist
+&soilyasso_namelist
+ ! param_y20_map=0.51, 5.19, 0.13, 0.1, 0.5, 0., 1., 1., 0.99, 0., 0., 0., 0., 0., 0.163, 0., 0., 0., 0., 0., 0., 0.158, -0.002, 0.17, -0.005, 0.067, 0.0, -1.44, -2.0, -6.9, 0.0042, 0.0015, -2.55, 1.24, 0.20
+ ! nc_mb=0.2
+ ! cue_min=0.1
+ ! nc_h_max=0.1
+ awenh_fineroot=0.46, 0.32, 0.04, 0.18, 0.0
+ awenh_leaf=0.46, 0.32, 0.04, 0.18, 0.0
+ awenh_soluble=0.0, 1.0, 0.0, 0.0, 0.0
+ awenh_compost=0.69, 0.09, 0.02, 0.20, 0.0
+ tempr_c=5.4
+ tempr_ampl=20
+ precip_day=1.87
+ totc=16.0
+ !cn_input=50
+ fract_root_input=0.4
+ fract_legacy_soc=0.3
+/ 
+EOF
+
+srun /users/tanghui1/SVMC/src/SVMC
+
+fi
+
+if [ ${run_case} == "F" ]
+then
+ 
+ module load geoconda
+ module load nco
+
+ cd /projappl/project_2006422/svm_data/output/veg_sensitivty/
+ 
+# for i in {368..368}; do
+#   cdo mergetime SVM_Qvidja.20*.day_vegsen_${i}.nc SVM_Qvidja_day_vegsen_${i}.nc
+#   cdo mergetime SVM_Qvidja.20*.hr_vegsen_${i}.nc SVM_Qvidja_hr_vegsen_${i}.nc
+
+#  for year in {2018..2022}; do
+#    ncks --quench --mk_rec_dmn time SVM_Qvidja.${year}.day_vegsen_${i}.nc test_day_${year}_${i}.nc
+#    ncks --quench --mk_rec_dmn time SVM_Qvidja.${year}.hr_vegsen_${i}.nc test_hr_${year}_${i}.nc
+#  done 
+#  ncrcat test_day_*_${i}.nc  SVM_Qvidja_day_vegsen_${expid}.nc
+#  ncrcat test_hr_*_${i}.nc  SVM_Qvidja_hr_vegsen_${expid}.nc
+
+#   rm test_day_*_${i}.nc
+#   rm test_hr_*_${i}.nc
+#   rm SVM_Qvidja.20*.day_vegsen_${i}.nc
+#   rm SVM_Qvidja.20*.hr_vegsen_${i}.nc
+
+# done
+ 
+ # Alternative way for fast processing using array job:
+# cdo mergetime SVM_Qvidja.20*.day_vegsen_${expid}.nc SVM_Qvidja_day_vegsen_${expid}.nc
+# cdo mergetime SVM_Qvidja.20*.hr_vegsen_${expid}.nc SVM_Qvidja_hr_vegsen_${expid}.nc
+
+  for year in {2018..2023}; do
+    ncks --quench --mk_rec_dmn time SVM_Qvidja.${year}.day_vegsen_${expid}.nc test_day_${year}_${expid}.nc
+    ncks --quench --mk_rec_dmn time SVM_Qvidja.${year}.hr_vegsen_${expid}.nc test_hr_${year}_${expid}.nc
+  done
+  ncrcat -O test_day_*_${expid}.nc  SVM_Qvidja_day_vegsen_${expid}.nc
+  ncrcat -O test_hr_*_${expid}.nc  SVM_Qvidja_hr_vegsen_${expid}.nc
+  
+  ncra -O --mro -d time,,,24,24 SVM_Qvidja_hr_vegsen_${expid}.nc SVM_Qvidja_hr-day_vegsen_${expid}.nc
+
+  rm test_day_*_${expid}.nc
+  rm test_hr_*_${expid}.nc
+
+# rm SVM_Qvidja.20*.day_vegsen_${expid}.nc
+# rm SVM_Qvidja.20*.hr_vegsen_${expid}.nc
+
+fi
+
+
+
+
+
